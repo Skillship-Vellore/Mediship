b51b11e308a4337f7afdeac9fc811f1d128a7ac9
Sai Sukesh <sukesh.sai04@gmail.com>
2021-04-12 21:07:40 +0530

    Initial commit

---
+++ app/src/main/java/com/skillship/mediship/ui/HomeFragment.java
@@ -0,0 +1,23 @@
+package com.skillship.mediship.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.skillship.mediship.R;
+
+public class HomeFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+
+        return root;
+    }
+}
